b905d940ed06ae6b7ef5e5e3ad21819637e75627
Yacine Ihadadene <ihadadene.yacine@gmail.com>
2021-07-13 21:24:31 +0200

    clean the repo

--- Section_1/convert.py
+++
@@ -1,7 +0,0 @@
-import base64
-
-val = "3q2+7w=="
-
-print(base64.b64decode(val).hex())
-
-
--- Section_1/json_cbor.py
+++
@@ -1,9 +0,0 @@
-import cbor2 as cbor
-
-v = 1
-
-for i in range (0, 19):
-    c = cbor.dumps(v)
-    print ("{0:3} {1:30} {2}".format(i, v, c.hex()))
-
-    v *= 10
